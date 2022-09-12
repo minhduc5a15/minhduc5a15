e120a4fb76b505c3006c9d56d7f3d45417eca745
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-12 20:54:26 +0700

    Update random.js

--- src/components/container/bubbles/random.js
+++ src/components/container/bubbles/random.js
@@ -3,17 +3,17 @@ import { styles } from "../../../styles";
 var random = [];
 var size = [40, 60];
 var sizeList = []
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 50; i++) {
      var sizeRandom = size[Math.floor(Math.random() * size.length)];
      random.push({
-          height: `${sizeRandom}px`,
           width: `${sizeRandom}px`,
+          height: `${sizeRandom}px`,
           left: `${Math.floor(Math.random() * 100)}%`,
           bottom: `${Math.floor(Math.random() * 100)}%`,
           animation: `${styles.circle} ${Math.random() * 23 + 10}s linear infinite`,
           animationDelay: `${Math.random() * 6}s`,
      });
      sizeList.push(sizeRandom);
-}
+};
 export { random, sizeList };
 
