59ddcac5d072ffb674877204f2d586da741ea879
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-12 19:37:26 +0700

    Update github.js

--- src/components/cardabout/button/github.js
+++ src/components/cardabout/button/github.js
@@ -6,8 +6,7 @@ const Github = () => {
      const [isHover, setIsHover] = useState(false);
      return (
           <div className={styles["button"]}>
-               <button
-                    onMouseEnter={() => setIsHover(true)}
+               <button onMouseEnter={() => setIsHover(true)}
                     onMouseLeave={() => setIsHover(false)}
                     onClick={() => window.open("https://github.com/minhduc5a15", "_blank")}>
                     <div>
