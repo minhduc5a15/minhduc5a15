8767deaa1016d6ea2580e189290626ae4ff50fed
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-13 20:01:13 +0700

    Update index.js

--- src/components/cardcontact/name/index.js
+++ src/components/cardcontact/name/index.js
@@ -1,16 +1,8 @@
-import { css } from "@emotion/css";
-import styled from "@emotion/styled";
+import { Container } from "../email";
 import { styles } from "../../../styles";
 import { SET_NAME } from "../../../provider/actions";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 
-const Container = styled.div`
-     width: 95%;
-     height: auto;
-     display: flex;
-     justify-content: center;
-     align-items: center;
-`;
 const Name = ({ name, onChange }) => {
      return (
           <div className={styles["box"]}
@@ -22,20 +14,8 @@ const Name = ({ name, onChange }) => {
                          <AccountCircle />
                     </div>
                     <div className={styles["box-input"]}>
-                         <input type="text" name="name" autoComplete="off" value={name}  maxLength={25} onChange={onChange(SET_NAME)} className={styles["box-text"]}
-                         />
-                         <label htmlFor="name" className={`${styles["box-label"]} ${css` ${name && `
-                                                  transform: translateY(-21px) translateX(7px);
-                                                  font-size: 13px;
-                                                  color: var(--box-input-border-focus);
-                                                  @media screen and (max-width: 575px) {
-                                                            transform: translateY(-21px) translateX(7px);
-                                                            font-size: 11px;
-                                                            color: var(--box-input-border-focus);       
-                                                  }
-                                             `}
-                                        `}
-                                   `}>
+                         <input type="text" name="name" autoComplete="off" value={name}  maxLength={25} onChange={onChange(SET_NAME)} className={styles["box-text"]} />
+                         <label htmlFor="name" className={`${styles["box-label"]} ${name && styles["box-label-valid"]}`}>
                               Name
                          </label>
                     </div>
