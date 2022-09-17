b1e0572b401e36b38958d0dc288f50bedf069bb0
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-18 00:05:09 +0700

    Update index.js

--- src/components/cardabout/heart/index.js
+++ src/components/cardabout/heart/index.js
@@ -2,44 +2,34 @@ import Loading from "../../loading";
 import { styles } from "../../../styles";
 import { rootRef } from "../../../firebase";
 import { useState, useEffect, useRef } from "react";
-import { KeyLocalStorage } from "../../../firebase/config";
 import { default as HeartIcon } from "@mui/icons-material/FavoriteBorderOutlined";
 import { default as HeartIconOutlined } from "@mui/icons-material/FavoriteOutlined";
-
+const getIsHeart = () => {
+     return window.localStorage.getItem("heart") || "false";
+};
 const Heart = () => {
      const [data, setData] = useState(0);
-     const [count, setCount] = useState(0);
-     const [value, setValue] = useState(0);
-     const [heart, setHeart] = useState("");
+     const [heart, setHeart] = useState(getIsHeart("heart"));
+     const [value, setValue] = useState(heart === "true" ? 1 : 0);
      const ref = useRef(rootRef["like"]);
-     useEffect(() => {
-          ref.current.on("value", (snapshot) => {
-               setCount(snapshot.val());
-          });
-          window.localStorage.getItem(KeyLocalStorage) && window.localStorage.removeItem(KeyLocalStorage);
-     }, []);
+     var heartCount = data;
      const Counter = async () => {
           if (heart !== "true") {
-               window.localStorage.setItem("isHeart", "true");
-               await ref.current.set(count + 1);
+               window.localStorage.setItem("heart", "true");
+               setHeart(getIsHeart("heart"));
+               await ref.current.set(heartCount + 1);
           }
           else {
-               window.localStorage.setItem("isHeart", "false");
-               await ref.current.set(count - value);
+               window.localStorage.setItem("heart", "false");
+               setHeart(getIsHeart("heart"));
+               await ref.current.set(heartCount - value);
           };
      };
      useEffect(() => {
           ref.current.on("value", (snapshot) => {
                setData(snapshot.val());
           });
-          setHeart(window.localStorage.getItem("isHeart"));
-          if (heart === "true") {
-               setCount(data);
-               setValue(1);
-          }
-          else {
-               setValue(0);
-          };
+          heart === "true" ? setValue(1) : setValue(0);
           return () => ref.current.off();
      }, [data]);
      return (
@@ -47,12 +37,12 @@ const Heart = () => {
                data-aos-duration="600"
                data-aos-delay="650"
                className={styles["heart-container"]}>
-               <div className={styles.heart} tabIndex="1">
+               <div className={styles["heart"]} tabIndex={1}>
                     <span onClick={Counter}>
                          {heart !== "true" ? <HeartIcon /> : <HeartIconOutlined style={{ color: "var(--heart-color-action)" }} />}
                     </span>
                </div>
-               <div className={styles.currentcount} style={{ cursor: data === 0 ? "progress" : "" }}>
+               <div className={styles.currentcount} style={{ cursor: data === 0 && "progress" }}>
                     {data === 0 ? <Loading /> : data}
                </div>
           </div>
