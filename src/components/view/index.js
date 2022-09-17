312d02dbd057d6a90d10b268fd430bed5d9849b0
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-17 22:42:37 +0700

    Update index.js

--- src/components/view/index.js
+++ src/components/view/index.js
@@ -1,35 +1,26 @@
 import Loading from "../loading";
 import { styles } from "../../styles";
 import { rootRef } from "../../firebase";
-import { VisitCountDb } from "../../firebase/config";
+import { useEffect, useState, useRef } from "react";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { useCallback, useEffect, useState, useRef } from "react";
 
 const View = () => {
      const [visitCount, setVisitCount] = useState(NaN);
      const ref = useRef(rootRef["view"]);
-     const UpdateVisitCount = useCallback(() => {
-          fetch(VisitCountDb)
-               .then((res) => res.json())
-               .then((res) => {
-                    ref.current.set(parseInt(res.value));
-               });
-          return;
-     }, []);
-     useEffect(() => {
-          UpdateVisitCount();
-     }, []);
+     var count = visitCount;
      useEffect(() => {
           ref.current.on("value", (snapshot) => {
                setVisitCount(snapshot.val());
           });
           return () => ref.current.off();
      }, []);
+     useEffect(() => {
+          !isNaN(count) && ref.current.set(count + 1);
+          return () => ref.current.off();
+     }, [count]);
      return (
           <div className={styles["view-box"]}>
-               <span>
-                    <VisibilityIcon />
-               </span>
+               <span><VisibilityIcon /></span>
                <span>{isNaN(visitCount) ? <Loading /> : visitCount}</span>
           </div>
      );
