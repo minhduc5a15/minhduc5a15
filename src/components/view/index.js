dba64e43eefd0df73e9f84e042c59112b9746e57
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-17 23:33:36 +0700

    Update index.js

--- src/components/view/index.js
+++ src/components/view/index.js
@@ -1,23 +1,30 @@
 import Loading from "../loading";
 import { styles } from "../../styles";
 import { rootRef } from "../../firebase";
-import { useEffect, useState, useRef } from "react";
+import { VisitCountDb } from "../../firebase/config";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 const View = () => {
      const [visitCount, setVisitCount] = useState(NaN);
      const ref = useRef(rootRef["view"]);
-     var count = visitCount;
+     const UpdateVisitCount = useCallback(() => {
+          fetch(VisitCountDb)
+               .then((res) => res.json())
+               .then((res) => {
+                    ref.current.set(parseInt(res.value));
+               });
+          return;
+     }, []);
+     useEffect(() => {
+          UpdateVisitCount();
+     }, []);
      useEffect(() => {
           ref.current.on("value", (snapshot) => {
                setVisitCount(snapshot.val());
           });
           return () => ref.current.off();
      }, []);
-     useEffect(() => {
-          !isNaN(count) && ref.current.set(count + 1);
-          return () => ref.current.off();
-     }, [count]);
      return (
           <div className={styles["view-box"]}>
                <span><VisibilityIcon /></span>
