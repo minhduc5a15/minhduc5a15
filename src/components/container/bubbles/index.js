42da9f7677383d5916774c597220b01284eb3ad7
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-17 22:54:31 +0700

    Update index.js

--- src/components/container/bubbles/index.js
+++ src/components/container/bubbles/index.js
@@ -1,62 +1,10 @@
 import { v4 } from "uuid";
 import { css } from "@emotion/css";
+import { styles } from "../../../styles";
 import { random, sizeList } from "./random";
 const Bubbles = () => {
      return (
-          <ul className={css`
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    z-index: -1;
-                    @media only screen and (max-width: 575px) {
-                         display: none;
-                    }
-                    & > li {
-                         position: absolute;
-                         list-style: none;
-                         display: block;
-                         background-color: var(--bubbles);
-                         bottom: -20px;
-                         opacity: 0;
-                         border-radius: 50%;
-                    }
-                    & > li::before {
-                         content: "";
-                         position: absolute;
-                         width: 10px;
-                         height: 10px;
-                         background: #fff;
-                         border-radius: 50%;
-                         filter: blur(2px);
-                    }
-                    & > li > span {
-                         inset: 5px;
-                         position: absolute;
-                         border-radius: 50%;
-                    }
-                    & > li > span:nth-child(1) {
-                         border-left: 5px solid #0051ff;
-                         filter: blur(3px);
-                    }
-                    & > li > span:nth-child(2) {
-                         border-right: 7px solid #ff008c;
-                         filter: blur(5px);
-                    }
-                    & > li > span:nth-child(3) {
-                         border-bottom: 5px solid #c2c2c2;
-                         filter: blur(3px);
-                    }
-                    & > li > span:nth-child(4) {
-                         border-top: 9px solid #d0e60d;
-                         filter: blur(5px);
-                    }
-                    & > li > span:nth-child(5) {
-                         border-left: 11px solid #ff008c;
-                         filter: blur(9px);
-                    }
-               `}>
+          <ul className={styles["bubble-list"]}>
                {Array.from({ length: random.length }).map((_, i) => {
                     return <li key={v4()} className={css`
                          &::before {
@@ -64,11 +12,9 @@ const Bubbles = () => {
                               left: ${sizeList[i] / 4}px;
                          }
                     `} style={random[i]}>
-                         <span></span>
-                         <span></span>
-                         <span></span>
-                         <span></span>
-                         <span></span>
+                         {Array.from({ length: 5 }).map((_) => {
+                              return <span key={v4()}></span>
+                         })}
                     </li>
                })}
           </ul>
