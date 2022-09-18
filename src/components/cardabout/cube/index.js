0d4c25e0340f515766225ba172252b2320e6505e
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-19 00:23:34 +0700

    Update index.js

--- src/components/cardabout/cube/index.js
+++ src/components/cardabout/cube/index.js
@@ -1,16 +1,23 @@
+import { v4 } from "uuid";
+import styled from "@emotion/styled";
 import { styles } from "../../../styles";
 
+const Side = styled.span`
+     transform: rotateY(${prop => prop.item}deg) translateZ(10px);
+     transition: 0.2s ease-in-out;
+     @media only screen and (max-width: 450px) {
+         transform: rotateY(${prop => prop.item}deg) translateZ(7.5px); 
+     }
+`;
 const Cube = () => {
      const Deg = [0, 90, 180, 270];
      return (
-          <div className={styles["cube-container"]}>
-               <div className={styles.cube}>
-                    <div className={styles.top}></div>
-                    <div>
-                         {Deg.map((item) => {
-                              return <span style={{ transform: `rotateY(${item}deg) translateZ(10px)` }} key={item}></span>
-                         })}
-                    </div>
+          <div className={styles.cube}>
+               <div className={styles.top}></div>
+               <div>
+                    {Deg.map((item) => {
+                         return <Side key={v4()} item={item}></Side>
+                    })}
                </div>
           </div>
      );
