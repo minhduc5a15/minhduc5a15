c7439c8fc36c72eb96f13fed0135432f7fbca7ca
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-18 23:47:00 +0700

    Update index.js

--- src/components/cardabout/wrapper/typing/index.js
+++ src/components/cardabout/wrapper/typing/index.js
@@ -2,8 +2,7 @@ import TypingBox from "./typingbox";
 import Typewriter from "typewriter-effect";
 
 const Typing = () => {
-     const introList = ["My name is Duc.", "I was born in 2005.", "I'm student.", "I'm web developer.", "I'm AI Engineer.",
-     ];
+     const introList = ["My name is Duc.", "I was born in 2005.", "I'm student.", "I'm web developer.", "I'm AI Engineer."];
      return (
           <TypingBox>
                <Typewriter options={{
@@ -11,8 +10,7 @@ const Typing = () => {
                     autoStart: true,
                     loop: true,
                     delay: 50,
-               }}
-               />
+               }} />
           </TypingBox>
      );
 };
