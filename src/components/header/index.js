32f0341dcc62c619f49fa79ea99a790850f2fb86
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-28 23:13:22 +0700

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,7 @@
 import View from "../view";
 import { styles } from "../../styles";
-import { styled } from "@mui/material/styles";
-import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import CustomWidthTooltip from "../tooltip";
 
-const CustomWidthTooltip = styled(({ className, ...props }) => (
-     <Tooltip {...props} classes={{ popper: className }} />
-))({
-     [`& .${tooltipClasses.tooltip}`]: {
-          width: 95,
-          height: 40,
-          color: "var(--tooltip-color)",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "16px",
-          background: "rgba(34, 34, 34, 0.3)",
-          letterSpacing: "1.5px",
-          fontWeight: "bold",
-          transform: "translateY(-10px)",
-     },
-});
 const Header = ({ onClick, icon, page }) => {
      return (
           <header>
