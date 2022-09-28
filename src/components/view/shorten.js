e23aa4aa8981b39d7d8716c5d4fce35e9ac55407
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-09-28 23:14:01 +0700

    Create shorten.js

---
+++ src/components/view/shorten.js
@@ -0,0 +1,13 @@
+const abbreviateNumber = (value) => {
+     let newValue = value;
+     const suffixes = ["", "K", "M", "B", "T"];
+     let suffixNum = 0;
+     while (newValue >= 1000) {
+          newValue /= 1000;
+          suffixNum++;
+     }
+     newValue = newValue.toPrecision(3);
+     newValue += suffixes[suffixNum];
+     return newValue;
+};
+export default abbreviateNumber;
